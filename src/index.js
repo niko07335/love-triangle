bc040b1cda5f2609039e5f85a0912d23e1d6391b
Nikita K <niko07335@gmail.com>
2019-03-14 00:28:22 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  preferences.splice(0, 0, 0);
+  let  counter = 0;
+  for (let i = 1; i < preferences.length; i++) {
+    let a = preferences[i];
+    if (preferences[a] !== i) {
+      let b = preferences[a];
+      if (preferences[b] !== a) {
+        let c = preferences[b];
+        if (preferences[c] !== b && c == i) {
+          counter++;
+        }
+      }
+    }
+  }
+  return(counter/3);
+}
